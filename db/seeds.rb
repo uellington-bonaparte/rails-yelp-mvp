d697c9d595ac25d970810fa738decff43dc83981
Uellington Cortes <code.uellington@gmail.com>
2020-11-27 16:54:06 -0300

    models implemented

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning DB...'
+Restaurant.destroy_all
+puts 'Db cleaned'
+puts 'Creating Restaurants'
+
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+
+  review = Review.new(
+    restaurant_id: restaurant.id,
+    content: Faker::Restaurant.review,
+    rating: rand(0..5)
+  )
+  puts "#{restaurant.name} is created"
+end
